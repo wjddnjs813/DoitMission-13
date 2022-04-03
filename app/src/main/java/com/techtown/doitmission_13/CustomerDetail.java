256cf8ed4d6dfab87960fe1c964e271308f46759
KWAK JEONGWON <5000batt@gmail.com>
2022-04-04 00:14:17 +0900

    상세화면 수정 및 삭제 버튼 클릭 생성(기능개발은 아직 안 됨)

--- app/src/main/java/com/techtown/doitmission_13/CustomerDetail.java
+++ app/src/main/java/com/techtown/doitmission_13/CustomerDetail.java
@@ -2,13 +2,15 @@ package com.techtown.doitmission_13;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CustomerDetail extends AppCompatActivity {
-    private Intent intent;
     String name;
     String birthDate;
     String phoneNum;
@@ -18,7 +20,7 @@ public class CustomerDetail extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customer_detail);
 
-        intent = getIntent();
+        Intent intent = getIntent();
         name = intent.getStringExtra("name");
         birthDate = intent.getStringExtra("birthDate");
         phoneNum = intent.getStringExtra("phoneNum");
@@ -30,5 +32,17 @@ public class CustomerDetail extends AppCompatActivity {
         editView.setText(name);
         editView2.setText(birthDate);
         editView3.setText(phoneNum);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext() ,MainActivity.class);
+
+                view.getContext().startActivity(intent);
+
+                Toast.makeText(view.getContext(), "수정되었습니다", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
